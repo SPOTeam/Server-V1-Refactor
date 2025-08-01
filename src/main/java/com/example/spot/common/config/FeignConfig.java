24b023ebd62be940c4e02d3337abc680e04a489c
msk226 <kminseok5167@gmail.com>
2025-08-01 13:52:44 +0900

    [SPOT-301][FEATURE] Feign 관련 Config 클래스 애노테이션 부착

--- src/main/java/com/example/spot/common/config/FeignConfig.java
+++ src/main/java/com/example/spot/common/config/FeignConfig.java
@@ -1,7 +1,9 @@
 package com.example.spot.common.config;
 
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
 
 @EnableFeignClients
+@Configuration
 public class FeignConfig {
 }
