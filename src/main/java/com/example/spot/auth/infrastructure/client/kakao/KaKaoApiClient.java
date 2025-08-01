f7a9e3ab974019e5dff8c1bb63507aa292d1cd2a
msk226 <kminseok5167@gmail.com>
2025-08-01 13:32:43 +0900

    [SPOT-301][FEATURE] 카카오 외부 API 호출 관련 Feign 로직 구현

---
+++ src/main/java/com/example/spot/auth/infrastructure/client/kakao/KaKaoApiClient.java
@@ -0,0 +1,18 @@
+package com.example.spot.auth.infrastructure.client.kakao;
+
+import static com.example.spot.auth.infrastructure.constants.AuthConstants.HEADER_AUTHORIZATION;
+import static com.example.spot.auth.infrastructure.constants.AuthConstants.HEADER_CONTENT_TYPE;
+
+import com.example.spot.auth.presentation.dto.KaKaoUser;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+@FeignClient(name = "kakaoApiClient", url = "https://kapi.kakao.com")
+public interface KaKaoApiClient {
+
+    @GetMapping("/v2/user/me")
+    KaKaoUser getKaKaoUserInfo(
+            @RequestHeader(HEADER_AUTHORIZATION) String accessToken,
+            @RequestHeader(HEADER_CONTENT_TYPE) String contentType);
+}
---
+++ src/main/java/com/example/spot/auth/infrastructure/client/kakao/KaKaoAuthClient.java
@@ -0,0 +1,21 @@
+package com.example.spot.auth.infrastructure.client.kakao;
+
+import static com.example.spot.auth.infrastructure.constants.AuthConstants.HEADER_CONTENT_TYPE;
+
+import com.example.spot.auth.presentation.dto.KaKaoOAuthTokenDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "kakaoAuthClient", url = "https://kauth.kakao.com")
+public interface KaKaoAuthClient {
+
+    @PostMapping("/oauth/token")
+    KaKaoOAuthTokenDTO getKaKaoAccessToken(
+            @RequestHeader(HEADER_CONTENT_TYPE) String contentType,
+            @RequestParam String grant_type,
+            @RequestParam String redirectUri,
+            @RequestParam String client_id,
+            @RequestParam(defaultValue = "authorization_code") String code);
+}
