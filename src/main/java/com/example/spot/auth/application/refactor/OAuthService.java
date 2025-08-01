9c0c7b3455027d5730d6ea38a5c72d8ec67eba56
msk226 <kminseok5167@gmail.com>
2025-08-01 14:35:59 +0900

    [SPOT-301][FEATURE] 전략 패턴 기반 OAuth 로그인 관련 서비스 클래스 정의

---
+++ src/main/java/com/example/spot/auth/application/refactor/OAuthService.java
@@ -0,0 +1,24 @@
+package com.example.spot.auth.application.refactor;
+
+import com.example.spot.auth.application.refactor.impl.OAuthMemberProcessor;
+import com.example.spot.member.domain.enums.LoginType;
+import com.example.spot.member.presentation.dto.MemberResponseDTO.SocialLoginSignInDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OAuthService {
+
+    private final OAuthStrategyFactory strategyFactory;
+    private final OAuthMemberProcessor memberProcessor;
+
+    public String redirectURL(LoginType type) {
+        return strategyFactory.getStrategy(type).getOauthRedirectURL();
+    }
+
+    public SocialLoginSignInDTO loginOrSignUp(LoginType type, String code) {
+        OAuthStrategy strategy = strategyFactory.getStrategy(type);
+        return memberProcessor.processOAuthMember(type, strategy.toMember(code));
+    }
+}
--- src/main/java/com/example/spot/auth/presentation/dto/GoogleUser.java
+++ src/main/java/com/example/spot/auth/presentation/dto/GoogleUser.java
@@ -1,6 +1,5 @@
 package com.example.spot.auth.presentation.dto;
 
-// 구글(서드파티)로 액세스 토큰을 보내 받아올 구글에 등록된 사용자 정보
 public record GoogleUser(
         String id,
         String email,
@@ -11,5 +10,5 @@ public record GoogleUser(
         String picture,
         String locale
 ) {
-    // 필요한 경우, 여기에 메서드 정의 가능 (예: toEntity)
+
 }
