0a1d20f4938ead3698ae93c447fa688355514c92
msk226 <kminseok5167@gmail.com>
2025-08-01 14:51:37 +0900

    [SPOT-301][FEATURE] Feign Client 스캔 Base Package 프로젝트 상위 디렉토리로 지정

--- src/main/java/com/example/spot/common/config/FeignConfig.java
+++ src/main/java/com/example/spot/common/config/FeignConfig.java
@@ -3,7 +3,7 @@ package com.example.spot.common.config;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
 
-@EnableFeignClients
+@EnableFeignClients(basePackages = "com.example.spot")
 @Configuration
 public class FeignConfig {
 }
